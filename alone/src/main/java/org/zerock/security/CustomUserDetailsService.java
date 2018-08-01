7da66a911c30d7752b0a62aec876524ba304188e
1027 <rana3782@gmail.com>
2018-08-01 20:54:20 +0900

    0801 security

--- alone/src/main/java/org/zerock/security/CustomUserDetailsService.java
+++ alone/src/main/java/org/zerock/security/CustomUserDetailsService.java
@@ -1,26 +1,41 @@
 package org.zerock.security;
 
 import java.util.Collections;
+import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.zerock.domain.MemberVO;
+import org.zerock.mapper.MemberMapper;
 
+import lombok.Setter;
 import lombok.extern.log4j.Log4j;
 
 @Log4j
 public class CustomUserDetailsService implements UserDetailsService {
 
+	
+	@Setter(onMethod_= {@Autowired})
+	private MemberMapper mapper;
+	
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 		log.info("loadUserByUsername"+username);
 		
+		MemberVO vo = mapper.read(username);
 		
+		User user = new User(vo.getUid(), vo.getUpw(), 
+				vo.getAuthList().stream().map(authVO->
+				new SimpleGrantedAuthority(authVO.getAuth())).collect(Collectors.toList()));
 		
-		return new User(username, "$2a$10$aka7ozjcGktTs2i0T3Z8EuXMZDjz8tKLvNlAb1o6OZlAzyGxpxw36", 
-				Collections.singletonList(new SimpleGrantedAuthority("ROLE_ADMIN")));
+		return user;
+		
+		/*return new User(username, "$2a$10$aka7ozjcGktTs2i0T3Z8EuXMZDjz8tKLvNlAb1o6OZlAzyGxpxw36", 
+				Collections.singletonList(new SimpleGrantedAuthority("ROLE_ADMIN")));*/
 	}
 
 }
